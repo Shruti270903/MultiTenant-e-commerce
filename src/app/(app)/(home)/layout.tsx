390250185932665da1671e43cc7bf1725665a23e
Shruti270903 <shrutikashyap2709@gmail.com>
2025-10-30 01:26:57 +0530

    commit categories finaliztion section

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,75 +1,11 @@
-// // "use client";  
-// "use server";
-// import configPromise from "@payload-config";
-// import { getPayload } from "payload";
-// import Navbar from "./navbar";
-// import { Footer } from "./footer";
-// import SearchFilters from "./search-filter";
-// import type { Category } from "@/payload-types";
-// interface props {
-//   children: React.ReactNode;
-// };
-// const Layout =async ({ children }: props) => {
-//   const payload=await getPayload({
-//     config: configPromise,
-//   })
-//   const data = await payload.find({
-//     collection:"categories" as any,
-//     depth:1,   //populate subcategories, subcategories.[0] will be a type of "category" 
-//     pagination:false,
-//     where:{
-//       parent:{
-//         exists:false,
-//       },
-//     },
-//   });
-//   interface SubCategory extends Category {
-//     subcategories: undefined;
-//   }
-
-//   interface FormattedCategory extends Category {
-//     subcategories: SubCategory[];
-//   }
-
-//   interface RawDoc extends Category {
-//     subcategories?: {
-//       docs: Category[];
-//     };
-//   }
-
-//   interface DocWithSubcategories extends Category {
-//     subcategories?: Category[];
-//   }
-
-//   const formattedData: FormattedCategory[] = (data.docs as RawDoc[]).map((doc: RawDoc): FormattedCategory => ({
-//     ...doc,
-//     subcategories: (doc.subcategories?.docs ?? []).map((doc: Category): SubCategory => ({
-//       ...(doc as Category),
-//       subcategories: undefined,
-//     })),
-//   }));
-//   console.log(data, formattedData);
-//   return (
-//     <div className="flex flex-col min-h-screen">
-//           <Navbar/>
-//           {/* <SearchFilters data={data}/> */}
-//           <SearchFilters data={data.docs} />
-//       <div className="flex-1 bg-[#F4F4F0]">  {children}</div>
-//       <Footer/>
-//           </div>
-//   );
-// };
-// export default Layout;
-
-
 "use server";
-
 import configPromise from "@payload-config";
 import { getPayload } from "payload";
 import Navbar from "./navbar";
 import { Footer } from "./footer";
 import SearchFilters from "./search-filter";
 import type { Category } from "@/payload-types";
+import { CustomCategory } from "./types";
 
 interface Props {
   children: React.ReactNode;
@@ -87,34 +23,16 @@ const Layout = async ({ children }: Props) => {
         exists: false, // only top-level categories
       },
     },
+    sort:"name"
   });
-
-  // --- TYPES ---
-  interface SubCategory extends Category {
-    subcategories?: undefined;
-  }
-
-  interface FormattedCategory extends Category {
-    subcategories: SubCategory[];
-  }
-
-  interface RawDoc extends Category {
-    subcategories?: {
-      docs: Category[];
-    };
-  }
-
   // --- TRANSFORM PAYLOAD DATA ---
-  const formattedData: FormattedCategory[] = (data.docs as RawDoc[]).map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
-    subcategories: (doc.subcategories?.docs ?? []).map((sub) => ({
-      ...sub,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      ...(doc as Category),
       subcategories: undefined, // prevent nesting loops
     })),
   }));
-
-  console.log("Formatted Data:", formattedData);
-
   // --- RENDER ---
   return (
     <div className="flex flex-col min-h-screen">
--- src/app/(app)/(home)/search-filter/index.tsx
+++ src/app/(app)/(home)/search-filter/index.tsx
@@ -1,18 +1,21 @@
 "use client";
+import { CustomCategory } from "../types";
 import { Categories } from "./categories";
 import { SearchInput } from "./search-input";
 
 
 interface Props {
-  data: any[];
+  data: CustomCategory[];
 }
 export default function SearchFilters({ data }: Props) {
   return (
     <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
-      <SearchInput/>
+      <SearchInput  data={data}/>
+      <div className="hidden lg:block">
       <Categories data={data}/>
       {/* <Categories data={data.categories}/> */}
       {/* {JSON.stringify(data, null, 2)} */}
+      </div>
     </div>
   );
 }
